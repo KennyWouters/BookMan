418c7acd5e5908a4bb849fab3b5989e577ca69ba
Kenny Wouters <kennywouters@divintage.com>
2025-02-10 16:05:13 +0100

    initial

--- server/index.js
+++ server/index.js
@@ -9,11 +9,11 @@ import * as path from "node:path"; // Import the email utility
 import session from "express-session";
 import bcrypt from "bcrypt";
 
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+// import { fileURLToPath } from 'url';
+// import { dirname } from 'path';
+//
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
 
 const app = express();
 const port = 3001;
