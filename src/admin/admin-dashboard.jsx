78dac2819988eebbd414208e71a8772a977e86b8
Kenny Wouters <kennywouters@divintage.com>
2025-02-12 00:43:43 +0100

    changed api url in admin login

--- src/admin/admin-dashboard.jsx
+++ src/admin/admin-dashboard.jsx
@@ -13,7 +13,12 @@ const AdminDashboard = () => {
     const fetchBookings = async (date) => {
         setLoading(true);
         try {
-            const formattedDate = date.toISOString().split('T')[0];
+            // const formattedDate = date.toISOString().split('T')[0];
+            const year = date.getFullYear();
+            const month = String(date.getMonth() + 1).padStart(2, '0');
+            const day = String(date.getDate()).padStart(2, '0');
+            const formattedDate = `${year}-${month}-${day}`;
+
             const response = await fetch(`https://book-man-b65d9d654296.herokuapp.com/api/admin/bookings?day=${formattedDate}`);
             if (response.ok) {
                 const data = await response.json();
